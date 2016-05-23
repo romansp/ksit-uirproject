c7c371eb0f4d41e441982c847965d9ce38cffa6c
Roman Pavlov <romanpavlv@gmail.com>
2016-05-23 16:13:18 +0300

    fix possible NRE in MasterPage

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -1,14 +1,13 @@
-using System;
+﻿using System;
 using System.Web;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (SiteMap.CurrentNode != null && 
-            SiteMap.CurrentNode.ToString() == SiteMap.RootNode.ToString() || SiteMap.CurrentNode.ToString() == string.Empty)
+        if (SiteMap.CurrentNode != null && SiteMap.RootNode != null && (SiteMap.CurrentNode.ToString() == SiteMap.RootNode.ToString() || SiteMap.CurrentNode.ToString() == string.Empty))
             Page.Title = "КСИТ / Павлов Роман";
-        else if (SiteMap.CurrentNode != null)
+        else if (SiteMap.CurrentNode != null && SiteMap.RootNode != null)
             Page.Title = SiteMap.CurrentNode + " / " + SiteMap.RootNode;
     }
 }
